15209cc0eca0bba3d06539c30785f151cca44b98
KyoungMIn YOO <91euriel@gmail.com>
2025-03-19 15:56:48 +0900

    fix: provider 수정

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -2,8 +2,7 @@
 
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-// <ReactQueryDevtools initialIsOpen={false} />
+
 const Provider = ({ children }: { children: React.ReactNode }) => {
   const queryClient = new QueryClient();
 
